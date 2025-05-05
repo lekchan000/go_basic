8b96d69e77f8186a716b710100b838e58af175a8
Suriya Thongsi <lekchan000@gmail.com>
2025-05-05 10:25:42 +0700

    Add launch configuration and main.go with loop examples

---
+++ loop/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	/* 	for i := 1; i <= 3; i++ {
+		println(i)
+	} */
+
+	var sum int
+	for i := 1; i <= 10; i++ {
+		sum += i
+	}
+
+	for i := range 3 {
+		println(i)
+	}
+}
