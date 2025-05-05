6eb74f84afda184ed5fffa09c1a8136446026d86
Suriya Thongsi <lekchan000@gmail.com>
2025-05-05 10:27:38 +0700

    Enhance main.go by adding fmt import and printing cumulative sum during iteration

--- loop/main.go
+++ loop/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	/* 	for i := 1; i <= 3; i++ {
 		println(i)
@@ -8,6 +10,7 @@ func main() {
 	var sum int
 	for i := 1; i <= 10; i++ {
 		sum += i
+		fmt.Println(sum)
 	}
 
 	for i := range 3 {
