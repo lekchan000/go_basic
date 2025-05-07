751c2003540ff6c6a513f42c445f0ecaad9ee0db
Suriya Thongsi <lekchan000@gmail.com>
2025-05-07 20:04:40 +0700

    Add main.go with PlayerClass interface and implementations for Warrior and Mage

---
+++ go-oop/polymorphism/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type (
+	PlayerClass interface {
+		Attack()
+	}
+
+	Warrior struct {
+		Weapon string
+	}
+
+	Mage struct {
+		Spell string
+	}
+)
+
+func (w Warrior) Attack() {
+	fmt.Println("Warrior attacks with", w.Weapon)
+}
+
+func (m Mage) Attack() {
+	fmt.Println("Mage casts", m.Spell)
+}
+
+func main() {
+	warrior := Warrior{
+		Weapon: "Sword",
+	}
+	mage := Mage{
+		Spell: "Fireball",
+	}
+
+	warrior.Attack()
+	mage.Attack()
+
+}
